97442d114ed49c56b3ce18ace818c125808ae424
Максим Румянцев <rummaksim@hotmail.com>
2023-01-28 17:30:30 +0500

    удалил лишнее из регрессии

--- MathLibrary/MathCalcs.cs
+++ MathLibrary/MathCalcs.cs
@@ -6,8 +6,8 @@ namespace MathLibrary
     {
         public double GetResult(double value)
         {
-            double[] inputs = { 80, 60, 10, 20, 30,36, 37 };
-            double[] outputs = { 20, 40, 30, 50, 60,66, 67 };
+            double[] inputs = { 80, 60, 10, 20, 30};
+            double[] outputs = { 20, 40, 30, 50, 60 };
 
             // Use Ordinary Least Squares to learn the regression
             OrdinaryLeastSquares ols = new OrdinaryLeastSquares();
