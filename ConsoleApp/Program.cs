559b9f51023e22b1b8b7bcf593aa3ba718739cdb
Максим Румянцев <rummaksim@hotmail.com>
2023-01-28 17:46:18 +0500

    1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,9 +5,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string s = "master change";
-        string s2 = "second change";
-        string s3 = "second second change";
         double value = 114;
         var lib = new MathCalcs();
         var result = lib.GetResult(value);
