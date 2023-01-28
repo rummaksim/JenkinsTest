8df2320dfb7b94ceace25af8684c197d9c777449
Максим Румянцев <rummaksim@hotmail.com>
2023-01-28 16:21:53 +0500

    master fix

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        int a = 1;
         double value = 50;
         var lib = new MathCalcs();
         var result = lib.GetResult(value);
