ad6d554498af678b2fbcf5aa054c8703a85aabc8
rummaksim <66171621+rummaksim@users.noreply.github.com>
2023-01-28 16:28:36 +0500

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,7 +5,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int a = 2;
         double value = 50;
         var lib = new MathCalcs();
         var result = lib.GetResult(value);
