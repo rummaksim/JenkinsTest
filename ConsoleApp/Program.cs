d4431a092ffd8675f5b21614ffbbc3eca637466d
rummaksim <66171621+rummaksim@users.noreply.github.com>
2023-01-28 17:35:52 +0500

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        string s = "master change";
         double value = 114;
         var lib = new MathCalcs();
         var result = lib.GetResult(value);
